f9db406ceaf9d5a0ac643cc2f1368e53ed70c9de
DdyYazo <alejoyaba@gmail.com>
2024-06-30 23:02:38 -0500

    🆕 🗃️ feat(*): Actualización index.html con manipulación basica del DOM

--- Practice_Playgrounds/array.js
+++
@@ -1,7 +0,0 @@
-/* En este desafío vas a recibir un parámetro arraySecreto en la función solution. Debes retornar true si el primer elemento del arraySecreto es de tipo string y debes retornar false si es de cualquier otro tipo. */
-
-// Utiliza el operador ternario para evaluar si el primer elemento del array es de tipo string y retornar true en caso afirmativo y false en caso contrario
-
-export function solution(arraySecreto) {
-    return typeof arraySecreto[0] === 'string' ? true : false;
-}
--- Practice_Playgrounds/return_typo.js
+++
@@ -1,9 +0,0 @@
-/* ¡Hola, Platzinauta! Por favor ignora la palabra export que agregamos antes de la función solution, la necesitamos para ejecutar las pruebas de tu código. Por ahora no te preocupes por eso, más adelante en tu ruta de aprendizaje aprenderás a profundidad cómo funciona. ¡Mucha suerte en tu reto! */
-
-export function solution(valor) {
-    return typeofvalor; // retorna el tipo de valor
-  }
-// llamar la función solution
-solution(5); // 'number'
-solution('Hola'); // 'string'
-solution(true); // 'boolean'
